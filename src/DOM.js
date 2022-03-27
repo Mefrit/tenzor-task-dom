9b174e3394d41c29e8118de3828b75e326f31a69
Mefrit <mihail_fokin_1999@mail.ru>
2022-03-27 18:37:16 +0300

    заврешение работы над заданиями с dom

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,13 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    let dom_elem;
+    for (let i = 0; i < count; i++) {
+        dom_elem = document.createElement(tag);
+        dom_elem.innerHTML = content;
+        document.body.appendChild(dom_elem);
+    }
+    return document;
 }
 
 /*
@@ -14,7 +21,29 @@ export function appendToBody(tag, content, count) {
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
-export function generateTree(childrenCount, level) {
+function addChildrenToTree(elem, children_count, curent_level, level) {
+    let child1 = document.createElement('div'),
+        child_cache = [];
+    child1.classList.add('item_' + curent_level);
+    for (let i = 0; i < children_count; i++) {
+        child_cache[i] = child1.cloneNode();
+        if (curent_level !== level) {
+            child_cache[i] = addChildrenToTree(
+                child_cache[i],
+                children_count,
+                curent_level + 1,
+                level,
+            );
+        }
+        elem.appendChild(child_cache[i]);
+    }
+    return elem;
+}
+export function generateTree(children_count, level) {
+    let parent = document.createElement('div');
+    parent.classList.add('item_' + 1);
+    parent = addChildrenToTree(parent, children_count, 2, level);
+    return parent;
 }
 
 /*
@@ -26,4 +55,16 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    let div = generateTree(2, 3),
+        section;
+    const leve_2_cache = div.getElementsByClassName('item_2');
+    for (let i = 0; i < leve_2_cache.length; i++) {
+        section = document.createElement('section');
+        section.setAttribute('class', leve_2_cache[i].getAttribute('class'));
+        for (let j = 0; j < leve_2_cache[i].childNodes.length; j++) {
+            section.appendChild(leve_2_cache[i].childNodes[j].cloneNode());
+        }
+        div.replaceChild(section, leve_2_cache[i]);
+    }
+    return div;
 }
